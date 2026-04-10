5d35560201716598de4e119e6623471c1facec5d
MikeBuckets2 <michaelmorris121@yahoo.com>
2026-04-10 22:21:57 +0000

    feat: add navbar for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,23 @@
-function Navbar() {
-  return <nav>Navbar</nav>;
+import '../styles/Navbar.css';
+
+function Navbar({ cartCount }) {
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar-logo">ShopReact</a>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/shop">Shop</a></li>
+        <li>
+          <a href="/cart" className="cart-link">
+            Cart
+            {cartCount > 0 && (
+              <span className="cart-badge">{cartCount}</span>
+            )}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
 export default Navbar;
