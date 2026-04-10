6a918b1869ea644d0785b13bc2e8389397f7c164
MikeBuckets2 <michaelmorris121@yahoo.com>
2026-04-10 22:17:29 +0000

    feat: set up browser router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,67 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import './styles/index.css';
+
 function App() {
-  return <div>Shopping Cart</div>;
+  const [cartItems, setCartItems] = useState([]);
+
+  function addToCart(product, quantity) {
+    const alreadyInCart = cartItems.find((item) => item.id === product.id);
+
+    if (alreadyInCart) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + quantity }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity }]);
+    }
+  }
+
+  function updateQuantity(id, newQuantity) {
+    if (newQuantity <= 0) {
+      setCartItems(cartItems.filter((item) => item.id !== id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: newQuantity } : item
+        )
+      );
+    }
+  }
+
+  function removeFromCart(id) {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  }
+
+  const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
+  return (
+    <BrowserRouter>
+      <Navbar cartCount={totalItems} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop addToCart={addToCart} />} />
+        <Route
+          path="/cart"
+          element={
+            <Cart
+              cartItems={cartItems}
+              updateQuantity={updateQuantity}
+              removeFromCart={removeFromCart}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
